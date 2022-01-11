7aaa7c4966d4a0ed528a81cb33e5b5c150ca765a
Tom Deseyn <tom.deseyn@gmail.com>
2022-01-11 09:15:02 +0100

    Support writing container types

--- src/Tmds.DBus.Protocol/Utf8Span.cs
+++ src/Tmds.DBus.Protocol/Utf8Span.cs
@@ -12,6 +12,8 @@ public ref struct Utf8Span
 
     public static implicit operator Utf8Span(ReadOnlySpan<byte> value) => new Utf8Span(value);
 
+    public static implicit operator Utf8Span(Span<byte> value) => new Utf8Span(value);
+
     public static implicit operator ReadOnlySpan<byte>(Utf8Span value) => value._buffer;
 
     public override string ToString() => Encoding.UTF8.GetString(_buffer);
