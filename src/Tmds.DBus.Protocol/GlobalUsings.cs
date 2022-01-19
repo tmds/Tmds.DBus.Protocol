9fbdfe5a56a5d18c62ba127fcadff2ed2d4a1b1c
Tom Deseyn <tom.deseyn@gmail.com>
2022-01-19 15:17:10 +0100

    Move global using to csproj

--- src/Tmds.DBus.Protocol/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using System.Buffers;
-global using System.Buffers.Binary;
-global using System.Diagnostics;
-global using Nerdbank.Streams;
-global using System.Runtime.CompilerServices;
-global using System.Runtime.InteropServices;
-global using System.Text;
